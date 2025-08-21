d020bc0e8a58f08023eab248a25c3662caea23c8
nelovick <139754249+nelovick@users.noreply.github.com>
2025-08-21 14:44:57 +0200

    ADDED ALL ITEMS

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
         if (item.type === "cell") {
           html = `
             <div class="accordion">
-              <div class="accordion-title">General Info</div>
+              <div class="accordion-title">General Information</div>
               <div class="accordion-content">
                 <p><b>Manufacturer:</b> ${item.manufacturer}</p>
                 <p><b>Model:</b> ${item.model}</p>
@@ -34,39 +34,99 @@
             </div>
 
           `;
-        } else if (item.type === "batpack") {
-          html = `
+        }else if (item.type === "batpack") {
+        html = `
+            <p><b>Battery Pack ID:</b> ${id}</p>
+            <p><b>Battery Passport ID:</b> ${item.passport_uuid}</p>          
+
             <div class="accordion">
-              <div class="accordion-title">Battery Pack ${id}</div>
-              <div class="accordion-content">
-                <p><b>UUID:</b> ${item.uuid}</p>
-                <p><b>Description:</b> ${item.description}</p>
-                <p><b>Total Cells:</b> ${item.total_cells}</p>
-                <p><b>Nominal Voltage:</b> ${item.nominal_voltage}</p>
-                <p><b>Total Capacity:</b> ${item.total_capacity}</p>
-              </div>
-              </div>
-                <div class="cell-grid">
-                  ${item.cells.map((cellId, i) => `<a href="index.html?id=${cellId}" class="cell-button">Cell ${i+1}</a>`).join("")}
-                </div>
-              
-            
-          `;
+            <div class="accordion-title">General Information</div>
+            <div class="accordion-content">
+                <p><b>Manufacturing Info:</b> ${item.general_information.manufacturing_info}</p>
+                <p><b>Battery Category:</b> ${item.general_information.battery_category}</p>
+                <p><b>Battery Weight:</b> ${item.general_information.battery_weight}</p>
+                <p><b>Battery Status:</b> ${item.general_information.battery_status}</p>
+            </div>
+            </div>
+
+            <div class="accordion">
+            <div class="accordion-title">Labels and Certifications</div>
+            <div class="accordion-content">
+                <p><b>Symbols & Labels:</b> ${item.labels_certifications.symbols_labels.join(", ")}</p>
+                <p><b>Meaning:</b> ${item.labels_certifications.meaning}</p>
+                <p><b>Declaration of Conformity:</b> ${item.labels_certifications.declaration_conformity}</p>
+                <p><b>Compliance of Test Results:</b> ${item.labels_certifications.test_compliance}</p>
+            </div>
+            </div>
+
+            <div class="accordion">
+            <div class="accordion-title">Carbon Footprint</div>
+            <div class="accordion-content">
+                <p><b>Carbon Footprint:</b> ${item.carbon_footprint.carbon_footprint}</p>
+                <p><b>Weblink to CF Study:</b> <a href="${item.carbon_footprint.weblink}" target="_blank">Study</a></p>
+                <p><b>CF Performance Class:</b> ${item.carbon_footprint.performance_class}</p>
+            </div>
+            </div>
+
+            <div class="accordion">
+            <div class="accordion-title">Supply Chain Due Diligence</div>
+            <div class="accordion-content">
+                <p><b>Due Diligence Report:</b> ${item.supply_chain.due_diligence_report}</p>
+            </div>
+            </div>
+
+            <div class="accordion">
+            <div class="accordion-title">Materials and Composition</div>
+            <div class="accordion-content">
+                <p><b>Hazardous Substances:</b> ${item.materials_composition.hazardous_substances}</p>
+                <p><b>Battery Chemistry:</b> ${item.materials_composition.battery_chemistry}</p>
+                <p><b>Critical Raw Materials:</b> ${item.materials_composition.critical_raw_materials.join(", ")}</p>
+                <p><b>Cathode:</b> ${item.materials_composition.materials_detailed.cathode}</p>
+                <p><b>Anode:</b> ${item.materials_composition.materials_detailed.anode}</p>
+                <p><b>Electrolyte:</b> ${item.materials_composition.materials_detailed.electrolyte}</p>
+            </div>
+            </div>
+
+            <div class="accordion">
+            <div class="accordion-title">Circularity & Resource Efficiency</div>
+            <div class="accordion-content">
+                <p><b>Recycled Content Shares:</b> ${item.circularity_resource_efficiency.recycled_content}</p>
+                <p><b>Manuals:</b> <a href="${item.circularity_resource_efficiency.manuals_disassembly}" target="_blank">Disassembly Manual</a></p>
+                <p><b>Spare Parts:</b> ${item.circularity_resource_efficiency.spare_parts}</p>
+                <p><b>Safety Measures:</b> ${item.circularity_resource_efficiency.safety_measures}</p>
+            </div>
+            </div>
+
+            <div class="accordion">
+            <div class="accordion-title">Performance & Durability</div>
+            <div class="accordion-content">
+                <p><b>Capacity / Energy / Power / SOH:</b> ${item.performance_durability.capacity_energy_power_soh}</p>
+                <p><b>Expected Lifetime:</b> ${item.performance_durability.expected_lifetime}</p>
+                <p><b>Negative Events:</b> ${item.performance_durability.negative_events}</p>
+            </div>
+            </div>
+
+            <div class="cell-grid">
+            ${item.cells.map((cellId, i) => `<a href="index.html?id=${cellId}" class="cell-button">Cell ${i+1}</a>`).join("")}
+            </div>
+        `;
         } else if (item.type === "bess") {
           html = `
+            <p><b>BESS ID:</b> ${id}</p>
+            <p><b>Battery Passport ID:</b> ${item.passport_uuid}</p>
             <div class="accordion">
-              <div class="accordion-title">BESS ID: ${id}</div>
-              <div class="accordion-content">
-                <p><b>UUID:</b> ${item.uuid}</p>
-                <p><b>Description:</b> ${item.description}</p>
-                <p><b>Location:</b> ${item.location}</p>
-                <p><b>Power:</b> ${item.power}</p>
-                <p><b>Energy:</b> ${item.energy}</p>
+              <div class="accordion-title">General Information</div>
+                <div class="accordion-content">
+                    <p><b>UUID:</b> ${item.uuid}</p>
+                    <p><b>Description:</b> ${item.description}</p>
+                    <p><b>Location:</b> ${item.location}</p>
+                    <p><b>Power:</b> ${item.power}</p>
+                    <p><b>Energy:</b> ${item.energy}</p>
                 </div>
-                </div>
-                <div class="pack-grid">
-                  ${item.batpacks.map((packId, i) => `<a href="index.html?id=${packId}" class="pack-button">Battery Pack ${i+1}</a>`).join("")}
-                </div>
+            </div>
+            <div class="pack-grid">
+                ${item.batpacks.map((packId, i) => `<a href="index.html?id=${packId}" class="pack-button">Battery Pack ${i+1}</a>`).join("")}
+            </div>
               
             
           `;
