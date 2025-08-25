f86f9dbb71350b22a3be099ab2b5cf42dbbf8897
nelovick <139754249+nelovick@users.noreply.github.com>
2025-08-25 10:35:45 +0200

    json modifieds

---
+++ js/script copy.js
@@ -0,0 +1,199 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+fetch("data.json")
+  .then(res => res.json())
+  .then(data => {
+    const content = document.getElementById("content");
+
+    if (id && data[id]) {
+      const item = data[id];
+      let html = "";
+
+      if (item.type === "cell") {
+        html = `
+          <div class="accordion">
+            <div class="accordion-title">General Information</div>
+            <div class="accordion-content">
+              <p><b>Manufacturer:</b> ${item.manufacturer}</p>
+              <p><b>Model:</b> ${item.model}</p>
+              <p><b>Manufacture Date:</b> ${item.manufacture_date}</p>
+              <p><b>Carbon Footprint:</b> ${item.carbon_footprint}</p>
+              <p><b>Capacity:</b> ${item.capacity}</p>
+              <p><b>Voltage:</b> ${item.voltage}</p>
+            </div>
+          </div>
+
+          <div class="accordion">
+            <div class="accordion-title">EOL Tests</div>
+            <div class="accordion-content">
+              <p><b>Voltage:</b> ${item.EOLvoltage}</p>
+              <p><b>Resistance:</b> ${item.EOLresistance}</p>
+              <p><b>Current:</b> ${item.EOLcurrent}</p>
+            </div>
+          </div>
+
+        `;
+      }else if (item.type === "batpack") {
+      html = `
+          <p><b>Battery Pack ID:</b> ${id}</p>
+          <p><b>Battery Passport ID:</b> ${item.passport_uuid}</p>          
+
+          <div class="accordion">
+          <div class="accordion-title">General Information</div>
+          <div class="accordion-content">
+              <p><b>Manufacturing Info:</b> ${item.general_information.manufacturing_info}</p>
+              <p><b>Battery Category:</b> ${item.general_information.battery_category}</p>
+              <p><b>Battery Weight:</b> ${item.general_information.battery_weight}</p>
+              <p><b>Battery Status:</b> ${item.general_information.battery_status}</p>
+          </div>
+          </div>
+
+          <div class="accordion">
+          <div class="accordion-title">Labels and Certifications</div>
+          <div class="accordion-content">
+              <p><b>Symbols & Labels:</b> ${item.labels_certifications.symbols_labels.join(", ")}</p>
+              <p><b>Meaning:</b> ${item.labels_certifications.meaning}</p>
+              <p><b>Declaration of Conformity:</b> ${item.labels_certifications.declaration_conformity}</p>
+              <p><b>Compliance of Test Results:</b> ${item.labels_certifications.test_compliance}</p>
+          </div>
+          </div>
+
+          <div class="accordion">
+          <div class="accordion-title">Carbon Footprint</div>
+          <div class="accordion-content">
+              <p><b>Carbon Footprint:</b> ${item.carbon_footprint.carbon_footprint}</p>
+              <p><b>Weblink to CF Study:</b> <a href="${item.carbon_footprint.weblink}" target="_blank">Study</a></p>
+              <p><b>CF Performance Class:</b> ${item.carbon_footprint.performance_class}</p>
+          </div>
+          </div>
+
+          <div class="accordion">
+          <div class="accordion-title">Supply Chain Due Diligence</div>
+          <div class="accordion-content">
+              <p><b>Due Diligence Report:</b> ${item.supply_chain.due_diligence_report}</p>
+          </div>
+          </div>
+
+          <div class="accordion">
+          <div class="accordion-title">Materials and Composition</div>
+          <div class="accordion-content">
+              <p><b>Hazardous Substances:</b> ${item.materials_composition.hazardous_substances}</p>
+              <p><b>Battery Chemistry:</b> ${item.materials_composition.battery_chemistry}</p>
+              <p><b>Critical Raw Materials:</b> ${item.materials_composition.critical_raw_materials.join(", ")}</p>
+              <p><b>Cathode:</b> ${item.materials_composition.materials_detailed.cathode}</p>
+              <p><b>Anode:</b> ${item.materials_composition.materials_detailed.anode}</p>
+              <p><b>Electrolyte:</b> ${item.materials_composition.materials_detailed.electrolyte}</p>
+          </div>
+          </div>
+
+          <div class="accordion">
+          <div class="accordion-title">Circularity & Resource Efficiency</div>
+          <div class="accordion-content">
+              <p><b>Recycled Content Shares:</b> ${item.circularity_resource_efficiency.recycled_content}</p>
+              <p><b>Manuals:</b> <a href="${item.circularity_resource_efficiency.manuals_disassembly}" target="_blank">Disassembly Manual</a></p>
+              <p><b>Spare Parts:</b> ${item.circularity_resource_efficiency.spare_parts}</p>
+              <p><b>Safety Measures:</b> ${item.circularity_resource_efficiency.safety_measures}</p>
+          </div>
+          </div>
+
+          <div class="accordion">
+          <div class="accordion-title">Performance & Durability</div>
+          <div class="accordion-content">
+              <p><b>Capacity / Energy / Power / SOH:</b> ${item.performance_durability.capacity_energy_power_soh}</p>
+              <p><b>Expected Lifetime:</b> ${item.performance_durability.expected_lifetime}</p>
+              <p><b>Negative Events:</b> ${item.performance_durability.negative_events}</p>
+          </div>
+          </div>
+
+          <img src="media/BatPack.png" usemap="#image-map">
+
+          <map name="image-map">
+              <area target="" alt="Cell10" title="Cell10" href="index.html?id=${item.cells[0]}" coords="126,106,561,296" shape="rect">
+              <area target="" alt="Cell9" title="Cell9" href="index.html?id=${item.cells[0]}" coords="128,324,565,515" shape="rect">
+              <area target="" alt="Cell8" title="Cell8" href="index.html?id=${item.cells[0]}" coords="133,544,562,734" shape="rect">
+              <area target="" alt="Cell7" title="Cell7" href="index.html?id=${item.cells[0]}" coords="134,765,573,950" shape="rect">
+              <area target="" alt="Cell6" title="Cell6" href="index.html?id=${item.cells[0]}" coords="135,987,562,1170" shape="rect">
+              <area target="" alt="Cell5" title="Cell5" href="index.html?id=${item.cells[0]}" coords="136,1203,562,1396" shape="rect">
+              <area target="" alt="Cell4" title="Cell4" href="index.html?id=${item.cells[0]}" coords="136,1423,564,1616" shape="rect">
+              <area target="" alt="Cell3" title="Cell3" href="index.html?id=${item.cells[0]}" coords="138,1643,570,1833" shape="rect">
+              <area target="" alt="Cell2" title="Cell2" href="index.html?id=${item.cells[0]}" coords="135,1863,565,2055" shape="rect">
+              <area target="" alt="Cell1" title="Cell1" href="index.html?id=${item.cells[0]}" coords="135,2082,564,2274" shape="rect">
+              <area target="" alt="Cell20" title="Cell20" href="index.html?id=${item.cells[0]}" coords="653,2081,1085,2274" shape="rect">
+              <area target="" alt="Cell19" title="Cell19" href="index.html?id=${item.cells[0]}" coords="648,1863,1081,2054" shape="rect">
+              <area target="" alt="Cell18" title="Cell18" href="index.html?id=${item.cells[0]}" coords="655,1645,1079,1836" shape="rect">
+              <area target="" alt="Cell17" title="Cell17" href="index.html?id=${item.cells[0]}" coords="653,1420,1082,1614" shape="rect">
+              <area target="" alt="Cell16" title="Cell16" href="index.html?id=${item.cells[0]}" coords="651,1203,1080,1399" shape="rect">
+              <area target="" alt="Cell15" title="Cell15" href="index.html?id=${item.cells[0]}" coords="654,982,1078,1177" shape="rect">
+              <area target="" alt="Cell14" title="Cell14" href="index.html?id=${item.cells[0]}" coords="656,765,1079,955" shape="rect">
+              <area target="" alt="Cell13" title="Cell13" href="index.html?id=${item.cells[0]}" coords="650,545,1079,734" shape="rect">
+              <area target="" alt="Cell12" title="Cell12" href="index.html?id=${item.cells[0]}" coords="653,323,1081,518" shape="rect">
+              <area target="" alt="Cell11" title="Cell11" href="index.html?id=${item.cells[0]}" coords="651,108,1084,294" shape="rect">
+          </map>
+
+      `;
+      } else if (item.type === "bess") {
+        html = `
+          <p><b>BESS ID:</b> ${id}</p>
+          <p><b>Battery Passport ID:</b> ${item.passport_uuid}</p>
+          <div class="accordion">
+            <div class="accordion-title">General Information</div>
+              <div class="accordion-content">
+                  <p><b>UUID:</b> ${item.uuid}</p>
+                  <p><b>Description:</b> ${item.description}</p>
+                  <p><b>Location:</b> ${item.location}</p>
+                  <p><b>Power:</b> ${item.power}</p>
+                  <p><b>Energy:</b> ${item.energy}</p>
+              </div>
+                <img src="media/BessString.png" usemap="#image-map">
+
+                <map name="image-map">
+                    <area target="" alt="BP1" title="BP1" href="index.html?id=${item.batpacks[0]}" coords="105,80,542,296" shape="rect">
+                    <area target="" alt="BP2" title="BP2" href="index.html?id=${item.batpacks[0]}" coords="109,340,537,557" shape="rect">
+                    <area target="" alt="BP3" title="BP3" href="index.html?id=${item.batpacks[0]}" coords="110,604,534,828" shape="rect">
+                    <area target="" alt="BP4" title="BP4" href="index.html?id=${item.batpacks[0]}" coords="112,869,533,1092" shape="rect">
+                    <area target="" alt="BP5" title="BP5" href="index.html?id=${item.batpacks[0]}" coords="112,1138,537,1360" shape="rect">
+                    <area target="" alt="BP6" title="BP6" href="index.html?id=${item.batpacks[0]}" coords="108,1400,534,1623" shape="rect">
+                    <area target="" alt="BP7" title="BP7" href="index.html?id=${item.batpacks[0]}" coords="105,1668,534,1890" shape="rect">
+                    <area target="" alt="BP8" title="BP8" href="index.html?id=${item.batpacks[0]}" coords="638,74,1066,299" shape="rect">
+                    <area target="" alt="BP9" title="BP9" href="index.html?id=${item.batpacks[0]}" coords="637,338,1066,561" shape="rect">
+                    <area target="" alt="BP10" title="BP10" href="index.html?id=${item.batpacks[0]}" coords="637,607,1070,828" shape="rect">
+                    <area target="" alt="BP11" title="BP11" href="index.html?id=${item.batpacks[0]}" coords="641,869,1067,1094" shape="rect">
+                    <area target="" alt="BP12" title="BP12" href="index.html?id=${item.batpacks[0]}" coords="642,1135,1068,1355" shape="rect">
+                    <area target="" alt="BP13" title="BP13" href="index.html?id=${item.batpacks[0]}" coords="642,1403,1060,1624" shape="rect">
+                    <area target="" alt="BP14" title="BP14" href="index.html?id=${item.batpacks[0]}" coords="636,1667,1060,1888" shape="rect">
+                </map>
+            
+          </map>
+            
+          
+        `;
+      }
+
+      // Back button if it has a parent
+      if (item.parent) {
+        html += `
+          <a href="index.html?id=${item.parent}" class="back-button">
+            ⬅ Back to ${item.parent}
+          </a>
+        `;
+      }
+
+      content.innerHTML = html;
+      // 🔹 Llama a imageMapResize tras crear el contenido dinámico
+    if (typeof imageMapResize === 'function') {
+      imageMapResize();
+    }
+
+    } else {
+      content.innerHTML = "<p>ID not found in the database.</p>";
+    }
+      // Activar los desplegables
+    document.querySelectorAll('.accordion-title').forEach(title => {
+      title.addEventListener('click', () => {
+        const content = title.nextElementSibling;
+        content.classList.toggle('open');
+        title.classList.toggle('active');  // añade/quita clase para la flecha
+      });
+    });
+  });
--- js/script.js
+++ js/script.js
@@ -1,45 +1,53 @@
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
-fetch("data.json")
-  .then(res => res.json())
-  .then(data => {
-    const content = document.getElementById("content");
+// Cargamos los tres JSON en paralelo
+Promise.all([
+  fetch("cells_data.json").then(res => res.json()),
+  fetch("bp_data.json").then(res => res.json()),
+  fetch("bess_data.json").then(res => res.json())
+]).then(([cellsData, batpacksData, bessData]) => {
+  const allData = { ...cellsData, ...batpacksData, ...bessData };
+  const content = document.getElementById("content");
 
-    if (id && data[id]) {
-      const item = data[id];
-      let html = "";
+  if (id && allData[id]) {
+    const item = allData[id];
+    let html = "";
 
-      if (item.type === "cell") {
-        html = `
-          <div class="accordion">
-            <div class="accordion-title">General Information</div>
-            <div class="accordion-content">
-              <p><b>Manufacturer:</b> ${item.manufacturer}</p>
-              <p><b>Model:</b> ${item.model}</p>
-              <p><b>Manufacture Date:</b> ${item.manufacture_date}</p>
-              <p><b>Carbon Footprint:</b> ${item.carbon_footprint}</p>
-              <p><b>Capacity:</b> ${item.capacity}</p>
-              <p><b>Voltage:</b> ${item.voltage}</p>
-            </div>
-          </div>
+    // Detectamos tipo
+    let type = "cell";
+    if (item.general_information) type = "batpack";
+    if (item.batpacks) type = "bess";
 
-          <div class="accordion">
-            <div class="accordion-title">EOL Tests</div>
-            <div class="accordion-content">
-              <p><b>Voltage:</b> ${item.EOLvoltage}</p>
-              <p><b>Resistance:</b> ${item.EOLresistance}</p>
-              <p><b>Current:</b> ${item.EOLcurrent}</p>
-            </div>
-          </div>
-
-        `;
-      }else if (item.type === "batpack") {
+    if (type === "cell") {
       html = `
-          <p><b>Battery Pack ID:</b> ${id}</p>
-          <p><b>Battery Passport ID:</b> ${item.passport_uuid}</p>          
+        <div class="accordion">
+          <div class="accordion-title">General Information</div>
+          <div class="accordion-content">
+            <p><b>Manufacturer:</b> ${item.GeneralInfo.manufacturer}</p>
+            <p><b>Model:</b> ${item.GeneralInfo.model}</p>
+            <p><b>Manufacture Date:</b> ${item.GeneralInfo.manufacture_date}</p>
+            <p><b>Carbon Footprint:</b> ${item.GeneralInfo.carbon_footprint}</p>
+            <p><b>Capacity:</b> ${item.GeneralInfo.capacity}</p>
+            <p><b>Voltage:</b> ${item.GeneralInfo.voltage}</p>
+          </div>
+        </div>
 
-          <div class="accordion">
+        <div class="accordion">
+          <div class="accordion-title">EOL Tests</div>
+          <div class="accordion-content">
+            <p><b>Voltage:</b> ${item.EolTest.EOLvoltage}</p>
+            <p><b>Resistance:</b> ${item.EolTest.EOLresistance}</p>
+            <p><b>Current:</b> ${item.EolTest.EOLcurrent}</p>
+          </div>
+        </div>
+      `;
+    } else if (type === "batpack") {
+      html = `
+        <p><b>Battery Pack ID:</b> ${id}</p>
+        <p><b>Battery Passport ID:</b> ${item.passport_uuid}</p>          
+
+        <div class="accordion">
           <div class="accordion-title">General Information</div>
           <div class="accordion-content">
               <p><b>Manufacturing Info:</b> ${item.general_information.manufacturing_info}</p>
@@ -47,142 +55,91 @@ fetch("data.json")
               <p><b>Battery Weight:</b> ${item.general_information.battery_weight}</p>
               <p><b>Battery Status:</b> ${item.general_information.battery_status}</p>
           </div>
-          </div>
+        </div>
 
-          <div class="accordion">
-          <div class="accordion-title">Labels and Certifications</div>
-          <div class="accordion-content">
-              <p><b>Symbols & Labels:</b> ${item.labels_certifications.symbols_labels.join(", ")}</p>
-              <p><b>Meaning:</b> ${item.labels_certifications.meaning}</p>
-              <p><b>Declaration of Conformity:</b> ${item.labels_certifications.declaration_conformity}</p>
-              <p><b>Compliance of Test Results:</b> ${item.labels_certifications.test_compliance}</p>
-          </div>
-          </div>
-
-          <div class="accordion">
-          <div class="accordion-title">Carbon Footprint</div>
-          <div class="accordion-content">
-              <p><b>Carbon Footprint:</b> ${item.carbon_footprint.carbon_footprint}</p>
-              <p><b>Weblink to CF Study:</b> <a href="${item.carbon_footprint.weblink}" target="_blank">Study</a></p>
-              <p><b>CF Performance Class:</b> ${item.carbon_footprint.performance_class}</p>
-          </div>
-          </div>
-
-          <div class="accordion">
-          <div class="accordion-title">Supply Chain Due Diligence</div>
-          <div class="accordion-content">
-              <p><b>Due Diligence Report:</b> ${item.supply_chain.due_diligence_report}</p>
-          </div>
-          </div>
-
-          <div class="accordion">
-          <div class="accordion-title">Materials and Composition</div>
-          <div class="accordion-content">
-              <p><b>Hazardous Substances:</b> ${item.materials_composition.hazardous_substances}</p>
-              <p><b>Battery Chemistry:</b> ${item.materials_composition.battery_chemistry}</p>
-              <p><b>Critical Raw Materials:</b> ${item.materials_composition.critical_raw_materials.join(", ")}</p>
-              <p><b>Cathode:</b> ${item.materials_composition.materials_detailed.cathode}</p>
-              <p><b>Anode:</b> ${item.materials_composition.materials_detailed.anode}</p>
-              <p><b>Electrolyte:</b> ${item.materials_composition.materials_detailed.electrolyte}</p>
-          </div>
-          </div>
-
-          <div class="accordion">
-          <div class="accordion-title">Circularity & Resource Efficiency</div>
-          <div class="accordion-content">
-              <p><b>Recycled Content Shares:</b> ${item.circularity_resource_efficiency.recycled_content}</p>
-              <p><b>Manuals:</b> <a href="${item.circularity_resource_efficiency.manuals_disassembly}" target="_blank">Disassembly Manual</a></p>
-              <p><b>Spare Parts:</b> ${item.circularity_resource_efficiency.spare_parts}</p>
-              <p><b>Safety Measures:</b> ${item.circularity_resource_efficiency.safety_measures}</p>
-          </div>
-          </div>
-
-          <div class="accordion">
-          <div class="accordion-title">Performance & Durability</div>
-          <div class="accordion-content">
-              <p><b>Capacity / Energy / Power / SOH:</b> ${item.performance_durability.capacity_energy_power_soh}</p>
-              <p><b>Expected Lifetime:</b> ${item.performance_durability.expected_lifetime}</p>
-              <p><b>Negative Events:</b> ${item.performance_durability.negative_events}</p>
-          </div>
-          </div>
-
-          <img src="media/BatPack.png" usemap="#image-map">
-
-          <map name="image-map">
-              <area target="" alt="Cell10" title="Cell10" href="index.html?id=${item.cells[0]}" coords="126,106,561,296" shape="rect">
-              <area target="" alt="Cell9" title="Cell9" href="index.html?id=${item.cells[0]}" coords="128,324,565,515" shape="rect">
-              <area target="" alt="Cell8" title="Cell8" href="index.html?id=${item.cells[0]}" coords="133,544,562,734" shape="rect">
-              <area target="" alt="Cell7" title="Cell7" href="index.html?id=${item.cells[0]}" coords="134,765,573,950" shape="rect">
-              <area target="" alt="Cell6" title="Cell6" href="index.html?id=${item.cells[0]}" coords="135,987,562,1170" shape="rect">
-              <area target="" alt="Cell5" title="Cell5" href="index.html?id=${item.cells[0]}" coords="136,1203,562,1396" shape="rect">
-              <area target="" alt="Cell4" title="Cell4" href="index.html?id=${item.cells[0]}" coords="136,1423,564,1616" shape="rect">
-              <area target="" alt="Cell3" title="Cell3" href="index.html?id=${item.cells[0]}" coords="138,1643,570,1833" shape="rect">
-              <area target="" alt="Cell2" title="Cell2" href="index.html?id=${item.cells[0]}" coords="135,1863,565,2055" shape="rect">
-              <area target="" alt="Cell1" title="Cell1" href="index.html?id=${item.cells[0]}" coords="135,2082,564,2274" shape="rect">
-              <area target="" alt="Cell20" title="Cell20" href="index.html?id=${item.cells[0]}" coords="653,2081,1085,2274" shape="rect">
-              <area target="" alt="Cell19" title="Cell19" href="index.html?id=${item.cells[0]}" coords="648,1863,1081,2054" shape="rect">
-              <area target="" alt="Cell18" title="Cell18" href="index.html?id=${item.cells[0]}" coords="655,1645,1079,1836" shape="rect">
-              <area target="" alt="Cell17" title="Cell17" href="index.html?id=${item.cells[0]}" coords="653,1420,1082,1614" shape="rect">
-              <area target="" alt="Cell16" title="Cell16" href="index.html?id=${item.cells[0]}" coords="651,1203,1080,1399" shape="rect">
-              <area target="" alt="Cell15" title="Cell15" href="index.html?id=${item.cells[0]}" coords="654,982,1078,1177" shape="rect">
-              <area target="" alt="Cell14" title="Cell14" href="index.html?id=${item.cells[0]}" coords="656,765,1079,955" shape="rect">
-              <area target="" alt="Cell13" title="Cell13" href="index.html?id=${item.cells[0]}" coords="650,545,1079,734" shape="rect">
-              <area target="" alt="Cell12" title="Cell12" href="index.html?id=${item.cells[0]}" coords="653,323,1081,518" shape="rect">
-              <area target="" alt="Cell11" title="Cell11" href="index.html?id=${item.cells[0]}" coords="651,108,1084,294" shape="rect">
-          </map>
-
+        <img src="media/BatPack.png" usemap="#image-map">
+        <map name="image-map">
+          ${item.cells.map((cellId, i) => `
+            <area target="" alt="Cell${i+1}" title="Cell${i+1}" href="index.html?id=${cellId}" coords="${getCellCoords(i)}" shape="rect">
+          `).join("")}
+        </map>
       `;
-      } else if (item.type === "bess") {
-        html = `
-          <p><b>BESS ID:</b> ${id}</p>
-          <p><b>Battery Passport ID:</b> ${item.passport_uuid}</p>
-          <div class="accordion">
-            <div class="accordion-title">General Information</div>
-              <div class="accordion-content">
-                  <p><b>UUID:</b> ${item.uuid}</p>
-                  <p><b>Description:</b> ${item.description}</p>
-                  <p><b>Location:</b> ${item.location}</p>
-                  <p><b>Power:</b> ${item.power}</p>
-                  <p><b>Energy:</b> ${item.energy}</p>
-              </div>
+    } else if (type === "bess") {
+      html = `
+        <p><b>BESS ID:</b> ${id}</p>
+        <p><b>Battery Passport ID:</b> ${item.passport_uuid}</p>
+        <div class="accordion">
+          <div class="accordion-title">General Information</div>
+          <div class="accordion-content">
+              <p><b>UUID:</b> ${item.uuid}</p>
+              <p><b>Description:</b> ${item.description}</p>
+              <p><b>Location:</b> ${item.location}</p>
+              <p><b>Power:</b> ${item.power}</p>
+              <p><b>Energy:</b> ${item.energy}</p>
           </div>
-                    <img src="media/BessString.png" usemap="#image-map">
+        </div>
 
-          <map name="image-map">
-            
-          </map>
-          <div class="pack-grid">
-              ${item.batpacks.map((packId, i) => `<a href="index.html?id=${packId}" class="pack-button">Battery Pack ${i+1}</a>`).join("")}
-          </div>
-            
-          
-        `;
-      }
-
-      // Back button if it has a parent
-      if (item.parent) {
-        html += `
-          <a href="index.html?id=${item.parent}" class="back-button">
-            ⬅ Back to ${item.parent}
-          </a>
-        `;
-      }
-
-      content.innerHTML = html;
-      // 🔹 Llama a imageMapResize tras crear el contenido dinámico
-    if (typeof imageMapResize === 'function') {
-      imageMapResize();
+        <img src="media/BessString.png" usemap="#image-map">
+        <map name="image-map">
+          ${item.batpacks.map((bpId, i) => `
+            <area target="" alt="BP${i+1}" title="BP${i+1}" href="index.html?id=${bpId}" coords="${getBPcoords(i)}" shape="rect">
+          `).join("")}
+        </map>
+      `;
     }
 
-    } else {
-      content.innerHTML = "<p>ID not found in the database.</p>";
+    // Botón volver dinámico
+    const parent = findParent(id, allData);
+    if (parent) {
+      html += `
+        <a href="index.html?id=${parent}" class="back-button">
+          ⬅ Back to ${parent}
+        </a>
+      `;
     }
-      // Activar los desplegables
+
+    content.innerHTML = html;
+
+    if (typeof imageMapResize === 'function') imageMapResize();
+
+    // Activar los desplegables
     document.querySelectorAll('.accordion-title').forEach(title => {
       title.addEventListener('click', () => {
         const content = title.nextElementSibling;
         content.classList.toggle('open');
-        title.classList.toggle('active');  // añade/quita clase para la flecha
+        title.classList.toggle('active');
       });
     });
-  });
+  } else {
+    content.innerHTML = "<p>ID not found in the database.</p>";
+  }
+});
+
+// 🔹 Helper para encontrar el padre
+function findParent(childId, allData) {
+  for (const [key, val] of Object.entries(allData)) {
+    if (val.cells && val.cells.includes(childId)) return key; // batpack -> cell
+    if (val.batpacks && val.batpacks.includes(childId)) return key; // bess -> batpack
+  }
+  return null;
+}
+
+// 🔹 Helpers para mantener tus coords originales
+function getCellCoords(index) {
+  const coords = [
+    "126,106,561,296","128,324,565,515","133,544,562,734","134,765,573,950","135,987,562,1170",
+    "136,1203,562,1396","136,1423,564,1616","138,1643,570,1833","135,1863,565,2055","135,2082,564,2274",
+    "651,108,1084,294","653,323,1081,518","650,545,1079,734","656,765,1079,955","654,982,1078,1177",
+    "651,1203,1080,1399","653,1420,1082,1614","655,1645,1079,1836","648,1863,1081,2054","653,2081,1085,2274"
+  ];
+  return coords[index] || "0,0,0,0";
+}
+
+function getBPcoords(index) {
+  const coords = [
+    "105,80,542,296","109,340,537,557","110,604,534,828","112,869,533,1092","112,1138,537,1360",
+    "108,1400,534,1623","105,1668,534,1890","638,74,1066,299","637,338,1066,561","637,607,1070,828",
+    "641,869,1067,1094","642,1135,1068,1355","642,1403,1060,1624","636,1667,1060,1888"
+  ];
+  return coords[index] || "0,0,0,0";
+}
